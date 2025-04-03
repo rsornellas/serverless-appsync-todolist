2a5bdf55ec423c2a37142f9f2cff9216769f74d7
Raphael Ornellas <raphaelso@live.com>
2025-04-03 00:43:17 -0300

    feat(resolvers): createTodo and updateTodo

--- mapping-templates/Mutation.updateTodo.js
+++ mapping-templates/Mutation.updateTodo.js
@@ -8,13 +8,13 @@ export function request(ctx) {
       id: { S: id }
     },
     update: {
-      expression: 'SET title = :title, description = :description, updatedAt = :updatedAt',
-      expressionNames: {
+      UpdateExpression: 'SET #title = :title, #description = :description, #updatedAt = :updatedAt',
+      ExpressionAttributeNames: {
         '#title': 'title',
         '#description': 'description',
         '#updatedAt': 'updatedAt'
       },
-      expressionValues: {
+      ExpressionAttributeValues: {
         ':title': { S: title },
         ':description': { S: description },
         ':updatedAt': { S: now }
